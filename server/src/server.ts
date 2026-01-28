7f7fe11248f79e601391a494a3d90fbdb8be35c3
gleidsonfagno <gleidsonfagner838@gmail.com>
2026-01-28 20:11:31 -0300

    Configuração de CORS adicionada para permitir requisições do frontend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,14 @@
 import express from "express"
 import { route } from "./routes"
+import cors from "cors";
 const app = express()
 const port = 3000
+
+app.use(cors({
+  origin: ["https://my-task-board-6whv.vercel.app/", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}))
 
 app.use(express.json())
 
